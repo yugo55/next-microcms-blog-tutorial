4950608a469f59db8f14ca308fc558fedc9a1a3c
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-07-22 18:19:15 +0900

    microcmsからデータの取得

---
+++ src/types/blog.ts
@@ -0,0 +1,20 @@
+export type Blog = {
+  id: string;
+  body: string;
+  title: string;
+  tags: Tag[];
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
+
+export type Tag = {
+  id: string;
+  tag: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
