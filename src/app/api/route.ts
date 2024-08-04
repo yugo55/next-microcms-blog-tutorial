c99be7f88981c4faa33b4e8f7993ae48458faf88
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-08-04 20:46:57 +0900

    microcmsでコンテンツの更新時にキャッシュの再検証

---
+++ src/app/api/route.ts
@@ -0,0 +1,48 @@
+import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
+import * as crypto from "crypto"
+
+export async function POST(request: Request): Promise<Response> {
+  console.log("Received webhook request");
+  const bodyText = await request.text();
+  const bodyBuffer = Buffer.from(bodyText, "utf-8");
+
+  const secret = process.env.MICROCMS_WEBHOOK_SIGNATURE_SECRET;
+  if (!secret) {
+    console.error("Secret is empty.");
+    return NextResponse.json({
+      status: 500,
+    });
+  }
+
+  const signature = request.headers.get("X-MICROCMS-Signature")
+  if (!signature) {
+    console.error("Signature is empty.");
+    return NextResponse.json({
+      status: 400,
+    });
+  }
+
+  const expectedSignature = crypto
+    .createHmac("sha256", secret)
+    .update(bodyBuffer)
+    .digest("hex");
+
+  const isValid = crypto.timingSafeEqual(
+    Buffer.from(signature),
+    Buffer.from(expectedSignature),
+  );
+
+  if (!isValid) {
+    console.error("Invalid signature.");
+    return NextResponse.json({
+      status: 400,
+    });
+  }
+
+  console.log("Signature verification passed");
+  revalidateTag("articles");
+  console.log("Revalidated articles tag");
+  
+  return NextResponse.json({ message: "success" })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,49 @@
 import { client } from "@/libs/client";
-import type { Blog, Tag } from '@/types/blog';
+import type { Blog, Tag } from "@/types/blog";
 import Link from "next/link";
 
 async function getData(): Promise<{ blogs: Blog[]; tags: Tag[] }> {
-  const blog = await client.get({ endpoint: "blog" });
-  const tag = await client.get({ endpoint: "tag" });
+  const blog = await client.get({
+    endpoint: "blog",
+    customRequestInit: {
+      // タグを設定
+      next: { tags: ["articles"] },
+    },
+  });
+  const tag = await client.get({
+    endpoint: "tag",
+    customRequestInit: {
+      // タグを設定
+      next: { tags: ["articles"] },
+    },
+  });
+
+  // blog.contents.forEach((b: Blog) => {
+  //   console.log(b.title)
+  // });
 
   return {
     blogs: blog.contents,
     tags: tag.contents,
   };
-};
+}
 
 export default async function Home() {
   const { blogs, tags } = await getData();
-  console.log(blogs);
-  console.log(tags)
+
+  // blogs.map((b) => {
+  //   console.log(b.body);
+  // });
 
   return (
     <div>
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              {blog.title}
-            </Link>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
           </li>
         ))}
       </ul>
     </div>
   );
-};
+}
